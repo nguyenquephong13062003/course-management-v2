818da2d83960a5a5a0fd162959c02a188e06aff3
Nguyen Que Phong <nguyenquephong13062003@gmail.com>
2026-07-08 01:23:53 +0700

    first commit

---
+++ src/main/java/com/example/course_management_v2/CourseManagementV2Application.java
@@ -0,0 +1,13 @@
+package com.example.course_management_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseManagementV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseManagementV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/course_management_v2/CourseManagementV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.course_management_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseManagementV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
